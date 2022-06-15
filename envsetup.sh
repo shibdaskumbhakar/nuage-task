956e2b83a459a15cc5e3fab3a6a76b2bf5979616
shibdas kumbhakar <37983176+shibdaskumbhakar@users.noreply.github.com>
2022-06-15 18:35:02 +0530

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -10,3 +10,13 @@ source env/bin/activate
 
 pip3 install -r requirements.txt
 
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
+
